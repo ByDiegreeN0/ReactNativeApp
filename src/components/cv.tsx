8e29f2d985a4b2af30648b1a68f7f74faa2da7cb
ByDiegreeN0 <diegoalejandrogarcia140902@gmail.com>
2024-07-30 17:29:55 -0500

    le meti lo q falta, pa q este mk tenga el repo actualizado

--- src/components/cv.tsx
+++ src/components/cv.tsx
@@ -1,10 +1,11 @@
-import { Text, View, Image, StyleSheet } from 'react-native'
+import { Text, View, Image, StyleSheet, ScrollView } from 'react-native'
 import React, { Component } from 'react'
 
 export const cv = () => {
     return (
+        
         <View style={style.container}>
-
+            <ScrollView>
             <View style={style.ImgContainer}>
                 <Image style={style.img} source={require('../../assets/img/React.png')}></Image>
             </View>
@@ -24,17 +25,17 @@ export const cv = () => {
 
                 <View style={style.Content} >
                     <Text style={style.ContentText}>Telefono</Text>
-                    <Text style={style.ContentText}>#############</Text>
+                    <Text style={style.ContentText}>+57 000 0000000</Text>
                 </View>
 
                 <View style={style.Content} >
                     <Text style={style.ContentText}>Email</Text>
-                    <Text style={style.ContentText}>###</Text>
+                    <Text style={style.ContentText}>diegoalejandrogarcia140902@gmail.com</Text>
                 </View>
 
                 <View style={style.Content} >
                     <Text style={style.ContentText}>Dirección</Text>
-                    <Text style={style.ContentText}>#####</Text>
+                    <Text style={style.ContentText}>cll 00a #00a-00</Text>
                 </View>
             </View>
 
@@ -43,6 +44,63 @@ export const cv = () => {
                 <Text></Text>
             </View>
 
+            <View>
+                <Text style={style.Header}>Skills</Text>
+
+                <View style={style.Content} >
+                    <Text style={style.ContentText}>Lenguaje</Text>
+                    <Text style={style.ContentText}>Años</Text>
+                </View>
+
+                <View style={style.Content} >
+                    <Text style={style.ContentText}>HTML</Text>
+                    <Text style={style.ContentText}>6</Text>
+                </View>
+
+                <View style={style.Content} >
+                    <Text style={style.ContentText}>CSS</Text>
+                    <Text style={style.ContentText}>6</Text>
+                </View>
+
+                <View style={style.Content} >
+                    <Text style={style.ContentText}>JavaScript</Text>
+                    <Text style={style.ContentText}>6</Text>
+                </View>
+
+                <View style={style.Content} >
+                    <Text style={style.ContentText}>React</Text>
+                    <Text style={style.ContentText}>2</Text>
+                </View>
+
+                <View style={style.Content} >
+                    <Text style={style.ContentText}>PHP</Text>
+                    <Text style={style.ContentText}>3</Text>
+                </View>
+
+                <View style={style.Content} >
+                    <Text style={style.ContentText}>Laravel</Text>
+                    <Text style={style.ContentText}>2</Text>
+                </View>
+
+                <View style={style.Content} >
+                    <Text style={style.ContentText}>Python</Text>
+                    <Text style={style.ContentText}>1</Text>
+                </View>
+
+                <View style={style.Content} >
+                    <Text style={style.ContentText}>Django</Text>
+                    <Text style={style.ContentText}>0.6</Text>
+                </View>
+
+                <View style={style.Content} >
+                    <Text style={style.ContentText}>Java</Text>
+                    <Text style={style.ContentText}>0.6</Text>
+                </View>
+            </View>
+
+            </ScrollView>
+
+           
 
 
         </View>
@@ -59,6 +117,8 @@ const style = StyleSheet.create({
     },
 
     ImgContainer: {
+        width: '100%',
+        height: '100%',
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
@@ -88,9 +148,9 @@ const style = StyleSheet.create({
     },
 
     ContentText: {
+        paddingTop: 15,
         fontSize: 16,
-        padding: 10,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
     }
 
 

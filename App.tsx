dfcaa2efca2973be5e961399cb4d632c78bf673d
ByDiegreeN0 <diegoalejandrogarcia140902@gmail.com>
2024-08-01 08:18:39 -0500

    se hicieron cositas x2

--- App.tsx
+++ App.tsx
@@ -1,13 +1,34 @@
 import { Text, View, StyleSheet, TextInput, Button, Image } from 'react-native'
 import React, { Component } from 'react'
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+
 import Login from './src/components/Login'
 import Register from './src/components/Register'
 import Cv from './src/components/cv'
 
+
+const Stack = createNativeStackNavigator();
+
+
 export const App = () => {
 
   return (  
-    <Cv/>
+
+      <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Register}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen name="Profile" component={Cv} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  
+  
   )
 
 }

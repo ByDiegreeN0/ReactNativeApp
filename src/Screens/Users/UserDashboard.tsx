bdd95edf0a1a12a1c971c137bac6a752f63ea93b
Diego <diegoalejandrogarcia140902@gmail.com>
2024-08-12 20:59:41 -0500

    jaja

---
+++ src/Screens/Users/UserDashboard.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class UserDashboard extends Component {
+  render() {
+    return (
+      <View>
+        <Text>UserDashboard</Text>
+      </View>
+    )
+  }
+}
+
+export default UserDashboard
